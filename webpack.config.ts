94d2f3d278aaf17fd750fe8b50cfa20e32491508
DocDuck <alex.mitin.psmu@gmail.com>
2023-05-31 08:18:01 +0500

    wip

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,12 +4,12 @@ import { getConfig } from './config/builder';
 import { IBuilderConfigOptions } from './config/builder/types/builder';
 
 const options: IBuilderConfigOptions = {
-mode: 'development',
-paths: {
-  output: path.resolve(__dirname, 'build'),
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  template: path.resolve(__dirname, 'public', 'index.html')
-}
+  mode: 'development',
+  paths: {
+    output: path.resolve(__dirname, 'build'),
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    template: path.resolve(__dirname, 'public', 'index.html')
+  }
 }
 
 const config: webpack.Configuration = getConfig(options);
