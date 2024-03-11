89c21780f0f34459b6797d38461f9a861f351e67
DocDuck <alex.mitin.psmu@gmail.com>
2024-03-11 12:07:34 +0500

    theme
    reset.css, add 3 themes, global css variables with fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,27 @@
 import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import cn from "classnames";
+import "../styles/index.scss"
 import s from "./styles.module.scss"
 
 const Home = lazy(() => import('../pages/home'));
 const About = lazy(() => import('../pages/about'));
 
 const App = () => (
-  <Router>
-    <div className={s.links}>
-      <Link to="/">to Main</Link>
-      <Link to="/about">to About</Link>
-    </div>
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Suspense>
-  </Router>
+  <div className={cn(s.app, "nord")}>
+    <Router>
+      <div className={s.links}>
+        <Link to="/">to Home</Link>
+        <Link to="/about">to About</Link>
+      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
+    </Router>
+  </div>
 );
 
 App.displayName = "App"
