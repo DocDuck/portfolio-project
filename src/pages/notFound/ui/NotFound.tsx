96602a07c89f158c4149df969b5f9bb6c4bc6d71
DocDuck <alex.mitin.psmu@gmail.com>
2024-05-02 17:46:20 +0500

    21 - прогон через линтеры

--- src/pages/notFound/ui/NotFound.tsx
+++ src/pages/notFound/ui/NotFound.tsx
@@ -1,8 +1,8 @@
-import { useTranslation } from 'react-i18next'
-import s from './notFound.module.scss'
+import { useTranslation } from 'react-i18next';
+import s from './notFound.module.scss';
 
 export const NotFound: React.FC = () => {
-  const { t } = useTranslation();
-  return (<div className={s.notFound}>{t("Страница не найдена")}</div>)
-}
-NotFound.displayName = "NotFound"
+	const { t } = useTranslation();
+	return (<div className={s.notFound}>{t("Страница не найдена")}</div>);
+};
+NotFound.displayName = "NotFound";
