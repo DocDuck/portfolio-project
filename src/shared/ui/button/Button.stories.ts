30eff442e893e3064eb5fa8d085b87aa5f5fc7ff
DocDuck <alex.mitin.psmu@gmail.com>
2024-07-30 16:24:20 +0500

    28 - btn refactoring, upd snapshots

--- src/shared/ui/button/Button.stories.ts
+++ src/shared/ui/button/Button.stories.ts
@@ -18,22 +18,70 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Clean: Story = {
+export const CleanS: Story = {
+	args: {
+		size: 's',
+		mode: 'clean',
+		children: 'CLEAN BTN'
+	},
+};
+
+export const CleanM: Story = {
 	args: {
 		mode: 'clean',
 		children: 'CLEAN BTN'
 	},
 };
 
-export const Border: Story = {
+export const CleanL: Story = {
+	args: {
+		size: 'l',
+		mode: 'clean',
+		children: 'CLEAN BTN'
+	},
+};
+
+export const BorderS: Story = {
+	args: {
+		size: 's',
+		mode: 'border',
+		children: 'BORDER BTN'
+	},
+};
+
+export const BorderM: Story = {
 	args: {
 		mode: 'border',
 		children: 'BORDER BTN'
 	},
 };
 
-export const Filled: Story = {
+export const BorderL: Story = {
 	args: {
+		size: 'l',
+		mode: 'border',
+		children: 'BORDER BTN'
+	},
+};
+
+export const FilledS: Story = {
+	args: {
+		size: 's',
+		mode: 'filled',
+		children: 'FILLED BTN'
+	},
+};
+
+export const FilledM: Story = {
+	args: {
+		mode: 'filled',
+		children: 'FILLED BTN'
+	},
+};
+
+export const FilledL: Story = {
+	args: {
+		size: 'l',
 		mode: 'filled',
 		children: 'FILLED BTN'
 	},
