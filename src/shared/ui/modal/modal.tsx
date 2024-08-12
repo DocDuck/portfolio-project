59a69d67807bd2c75190cc7fbdb01e27673a3dbc
DocDuck <alex.mitin.psmu@gmail.com>
2024-08-12 20:07:14 +0500

    29 - open/close modal

--- src/shared/ui/modal/modal.tsx
+++ src/shared/ui/modal/modal.tsx
@@ -1,18 +1,36 @@
-import { ReactNode } from "react";
+import { MouseEvent, ReactNode, useCallback } from "react";
 import { Portal } from "shared/ui/portal";
 import "./modal.scss";
+import { cn } from "shared/lib/classNames";
+import { log } from "console";
 
 interface IModalProps {
-  children: ReactNode;
+	children: ReactNode;
+	className?: string;
+	isOpen?: boolean;
+	onClose?: () => void;
 }
 
 export const Modal: React.FC<IModalProps> = (props) => {
-	const { children } = props;
-
+	const { children, className = '', isOpen = false, onClose } = props;
+	console.log('isOpen', isOpen)
+	const onOutsideClick = useCallback(() => {
+		onClose && onClose();
+	}, [onClose]);
+	const onContentClick = (e: MouseEvent) => e.stopPropagation(); 
 	return (
 		<Portal>
-			<div className="modal">
-				<div className="content">
+			<div
+				className={cn('modal', {
+					[className]: !!className,
+					'open': isOpen
+				})}
+				onClick={onOutsideClick}
+			>
+				<div
+					className="content"
+					onClick={onContentClick}
+				>
 					{children}
 				</div>
 			</div>
