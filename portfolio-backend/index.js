60009322a80da1d86b926b2b9114f508d20b3f26
DocDuck <34368922+DocDuck@users.noreply.github.com>
2024-09-04 19:03:40 +0500

    feature/31 - add backend (#5)
    * 31 - test monorepo with github actions
    
    * 31 - upd working-directory
    
    * 31 - simple backend express server

---
+++ portfolio-backend/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = 8000;
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello world!')
+});
+
+app.listen(port, () => {
+  console.log(`server running at localhost:${port}`)
+})
