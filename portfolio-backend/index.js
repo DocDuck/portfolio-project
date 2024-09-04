be6e0bed845ee008a7807f597953933a189b640d
DocDuck <alex.mitin.psmu@gmail.com>
2024-09-04 18:49:26 +0500

    31 - simple backend express server

--- portfolio-backend/index.js
+++ portfolio-backend/index.js
@@ -1,2 +1,13 @@
-import http from "http";
+import express from "express";
 
+const port = 8000;
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello world!')
+});
+
+app.listen(port, () => {
+  console.log(`server running at localhost:${port}`)
+})
